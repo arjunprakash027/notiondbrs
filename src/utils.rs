d369909678b8952a28e7eb26fee225a0e24ff439
Arjun Prakash <arjunprakash027@gmail.com>
2025-08-03 19:08:48 +0530

    get all databases from notion

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,10 @@
 use polars::prelude::*;
 
-pub fn create_df() -> PolarsResult<()> {
+pub fn create_df() -> PolarsResult<DataFrame> {
     let s = Series::new("companies".into(), &["google","microsoft","apple"]);
     let c = Series::new("market_cap".into(), &[1,2,3]);
     
-    let df = DataFrame::new(vec![s.into(),c.into()])?;
+    let df = DataFrame::new(vec![s.into(),c.into()])?;    
     
-    println!("{:?}", df);
-    
-    Ok(())
+    Ok(df)
 }
